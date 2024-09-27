ef5343746edb8151c9b330d44bfde95e1957bf42
JefersondaCruz <jefersondacruz40@gmail.com>
2024-09-27 13:15:21 -0300

    FirstCommmit

---
+++ src/app/Http/Controllers/Admin/SupportController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Support;
+// use GuzzleHttp\Psr7\Request;
+use Illuminate\Http\Request;
+
+class SupportController extends Controller
+{
+    public function index (Support $support)
+    {
+        $supports = $support->all();
+        // dd($supports);
+
+
+        return view('admin/supports/index', compact('supports'));
+    }
+
+    public function create()
+    {
+        return view('admin/supports/create');
+    }
+
+    public function store(Request $request, Support $support)
+    {
+        $data = $request->all();
+        $data ['status'] = 'a';
+
+        $support->create( $data );
+
+        return redirect()->route('supports.index');
+
+    }
+}
