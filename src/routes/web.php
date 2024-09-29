1cf36bec6d652d5cc93d693889197be9b6713190
JefersondaCruz <jefersondacruz40@gmail.com>
2024-09-29 20:19:49 -0300

    NewFeatures

--- src/routes/web.php
+++ src/routes/web.php
@@ -4,20 +4,15 @@ use App\Http\Controllers\Site\SiteController;
 use App\Http\Controllers\Admin\{SupportController};
 use Illuminate\Support\Facades\Route;
 
+Route::put("/supports/{id}", [SupportController::class,"update"])->name("supports.update");
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-Route::post('/supports', [SupportController::class,'store'])->name('supports.store');
+Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
 
 Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+
+Route::get('/supports/{id}', [SupportController::class,'show'])->name('supports.show');
+
+Route::post('/supports', [SupportController::class,'store'])->name('supports.store');
 
 Route::get('/supports',[SupportController::class, 'index'])->name('supports.index');
 
