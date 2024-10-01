ccfce5812ca988404580e0b47ff53073960b4a65
JefersondaCruz <jefersondacruz40@gmail.com>
2024-10-01 14:28:20 -0300

    feat: implements store and update repository

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Repositories\SupportRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
+use SupportEloquentORM;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(SupportRepositoryInterface::class)
+        $this->app->bind(
+            SupportRepositoryInterface::class,
+        SupportEloquentORM::class
+        );
     }
 
     /**
